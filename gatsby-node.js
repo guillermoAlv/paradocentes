c598ad9fc7465124e382df53f6186ad65c075c4e
Guillermo Alvarez <guillermoalvma@gmail.com>
2021-01-27 12:50:44 +0100

    base project

---
+++ gatsby-node.js
@@ -0,0 +1,44 @@
+exports.createPages = async ({ actions: { createPage },  graphql}) => {
+  console.log("holaa")
+  createPage({
+    path: "/no-data/",
+    component: require.resolve("./src/templates/no-data.js"),
+  })
+
+  createPage({
+      path: "/page-with-context/",
+      component: require.resolve("./src/templates/with-context.js"),
+      context: {
+        title: "We Don’t Need No Stinkin’ GraphQL!",
+        content: "<p>This is page content.</p><p>No GraphQL required!</p>",
+      },
+    })
+  
+    const result = await graphql(`
+    {
+      graphql {
+        pages {
+          nodes {
+            id
+            uri
+          }
+        }
+      }
+    }
+  `)
+
+  // pull the page data out of the query response
+  const pages = result.data.graphql.pages.nodes
+
+  // loop through WordPress pages and create a Gatsby page for each one
+  pages.forEach(page => {
+    console.log(page)
+    createPage({
+      path: page.uri,
+      component: require.resolve("./src/templates/page-template.js"),
+      context: {
+        id: page.id,
+      },
+    })
+  })
+}
